56e51004bae64bda5e2b21fa988d532d3673670f
Laura Bullek <74018395+Laura-Bullek@users.noreply.github.com>
2021-01-22 11:50:03 -0500

    Put randomization of letters, numbers and characters into an object

--- javascript.js
+++ javascript.js
@@ -1,7 +1,20 @@
-const lowerCase = "abcdefghijklmnopqrstuvwxyz"; // String of lowercase characters
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //String of uppercase letters
-const numbers = "0123456789"; //String of numbers
-const specCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"; //String of special characters
+// Putting all of the randomization of letters, numbers and
+// special characters into an object
+
+const randomFuncions = {
+    lower : randomLowerCase,
+    upper: randomUpperCase,
+    number: randomNumbers,
+    specCharacters: randomSpecialCharacters
+};
+
+
+
+
+const genLowerCase = "abcdefghijklmnopqrstuvwxyz"; // String of lowercase characters
+const genUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //String of uppercase letters
+const genNumbers = "0123456789"; //String of numbers
+const getSpecCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"; //String of special characters
 
 // Randomizing each string of letters, numbers, and special characters
 const randomLowerCase = lowerCase [Math.floor(Math.random() * lowerCase.length)];
@@ -10,6 +23,7 @@ const randomNumbers = numbers [Math.floor(Math.random() * numbers.length)];
 const randomSpecialCharacters = specCharacters[Math.floor(Math.random() * specCharacters.length)];
 
 // Adding all of the variables together to make a random string, one of each variable
-console.log(randomLowerCase);
+console.log(randomSpecialCharacters);
 
-      
+
+// Slider code
